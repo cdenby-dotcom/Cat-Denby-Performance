162c6a107b1d6e3627db6b0e8817c237c0d470fe
cdenby-dotcom <cattidee@gmail.com>
2026-09-03 10:20:43 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,13 @@
 'use client';
-import Image from "next/image";
+
 import Hero from "./components/hero/Hero";
 import Nav from "./components/Nav/Nav";
 import About from "./components/About/About";
 import ClientSection from "./components/ClientSection/ClientSection";
 import ContactSection from "./components/ContactPage/ContactSection";
 import { ScrollContext } from "./contexts/scrollContext";
-import { useEffect, useRef, useState } from "react";
-import { FaRegCopyright } from "react-icons/fa";
+import { useRef } from "react";
 import Services from "./components/Services/Services";
-import gsap from "gsap"
-import { useGSAP } from '@gsap/react';
 
 export default function Home() {
 
@@ -20,38 +17,26 @@ export default function Home() {
   const clientsRef = useRef<HTMLDivElement | null>(null);
   const contactRef = useRef<HTMLDivElement | null>(null);
 
-  const [opacites, setOpacities] = useState([0,0,0,0])
-
-
-  useGSAP(() => {
-    gsap.fromTo(
-      '.Header',
-      {y: "-100%", autoAlpha: 0},
-      {y: 0, autoAlpha: 1, duration: 0.8, ease: "power2.out"
-      }
-    )
-  })
-  
   return (
     <ScrollContext.Provider value={{ homeRef, aboutRef, servicesRef, clientsRef, contactRef }}>
-    <div className="page">
-            <div className="aboutOverlay"></div>
+      <div className="page">
+        <div className="aboutOverlay"></div>
 
-      <header className="Header" id="Header">
-        <Nav header={true}/>
-        <div className="borderBottonDiv"></div>
+        <header className="Header" id="Header">
+          <Nav header={true} />
+          <div className="borderBottonDiv"></div>
         </header>
-        
-       
-      <Hero />
-       <About opacity={opacites[0]} />
-       <Services />
 
-      <ClientSection />
-      <ContactSection />
-      <footer className="Footer"><Nav header={false} /></footer>
-      {/* <Footer /> */}
-    </div>
+        <Hero />
+        <About />
+        <Services />
+        <ClientSection />
+        <ContactSection />
+
+        <footer className="Footer">
+          <Nav header={false} />
+        </footer>
+      </div>
     </ScrollContext.Provider>
   );
 }
